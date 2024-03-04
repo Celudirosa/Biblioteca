6e299c729f594fda70ad5bfdcc38dfc89ce61c80
celudirosa <cluqued@live.com>
2024-03-04 21:20:14 +0100

    update libro service

--- src/main/java/com/example/services/LibroService.java
+++ src/main/java/com/example/services/LibroService.java
@@ -1,22 +1,26 @@
 package com.example.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import com.example.entities.Autor;
 import com.example.entities.Libro;
 
 public interface LibroService {
 
-    public List<Libro> findById(int idAutor);
-    public List<Libro> findLibrosByAutorId(int idAutor);
-
-    public List<Libro> findAll();
-    public Libro save(Libro libro);
-    Libro findFirstById(int id);
-    public boolean existById(Integer libroId);
+    List<Libro> findAll();
     Page<Libro> findAll(Pageable pageable);
-    public void deleteById(Integer id);
+    Page<Libro> findAll(Sort sort);
+
+    Libro findById(int id);
+    List<Libro> findLibrosByAutorId(int id);
+
+    Libro save(Libro libro);
+    void delete(Libro libro);
+    List<Libro> findByAutorAndDate(Autor autor, LocalDate date);
 
 }
