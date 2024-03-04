9c31104b81563d55a993ab79492abc7b0c64e29f
celudirosa <cluqued@live.com>
2024-03-04 10:22:17 +0100

    add lists to repositories

--- src/main/java/com/example/repository/AutorRepository.java
+++ src/main/java/com/example/repository/AutorRepository.java
@@ -1,9 +1,12 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entities.Autor;
 
 public interface AutorRepository extends JpaRepository<Autor, Integer> {
 
+    List<Autor> findAutoresByLibrosId(Integer libroId);
 }
--- src/main/java/com/example/repository/LibroRepository.java
+++ src/main/java/com/example/repository/LibroRepository.java
@@ -1,9 +1,15 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.entities.Libro;
 
 public interface LibroRepository extends JpaRepository<Libro, Integer> {
+
+    List<Libro> findLibrosByAutoresId(Integer autorId);
+
+    List<Libro> findByTitulo(String titulo);
 
 }
