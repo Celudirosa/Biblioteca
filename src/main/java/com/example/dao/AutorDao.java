5da210d9c046c9d60c82c7c71fb14f7ab1c514b8
celudirosa <cluqued@live.com>
2024-03-04 21:11:16 +0100

    update DAOs

--- src/main/java/com/example/dao/AutorDao.java
+++ src/main/java/com/example/dao/AutorDao.java
@@ -1,6 +1,7 @@
 package com.example.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,16 @@ import com.example.entities.Autor;
 @Repository
 public interface AutorDao extends JpaRepository<Autor, Integer> {
 
-    List<Autor> findAutoresByLibrosId(Integer libroId);
-    Autor findById(int id);
+    // Método para encontrar un autor por su ID
+    Optional<Autor> findById(int id);
+
+    // Método para encontrar un autor por su ID (otra versión)
+    Autor findAutorById(int id);
+
+    // Método para encontrar autores por el ID de un libro
+    List<Autor> findByLibrosId(int libroId);
+
+    // TODO: Este metodo es necesario?
     List<Autor> findByNombreContaining(String nombre);
 
 }
--- src/main/java/com/example/dao/LibroDao.java
+++ src/main/java/com/example/dao/LibroDao.java
@@ -1,24 +1,23 @@
 package com.example.dao;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import com.example.entities.Libro;
 import com.example.entities.Autor;
+import com.example.entities.Libro;
 
 
 public interface LibroDao extends JpaRepository<Libro, Integer> {
 
-    List<Libro> findByAutores(Autor autor);
+    // Método para encontrar un libro por su ID
+    Libro findById(int id);
 
-    List<Libro> findById(int id);
+    // Método para encontrar libros por el ID de un autor
+    List<Libro> findByIdAutores(int id);
 
-    Libro findFirstById(int id);
-
-    List<Libro> findLibrosByAutoresId(Integer autorId);
+    // Método para encontrar libros por el ID de un autor y una fecha específica
+    List<Libro> findByAutoresIdAndDate(Autor autor, LocalDate date);
 
 }
